2e43f1d90323b129a713258d073d150a800e5d58
pa-de-solminihac <pa@quai13.com>
2018-08-24 16:21:00 +0200

    fix(install): basic checks in install.sh and update.sh

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+[ -d "~/.config/config-pa-de-solminihac" ] && echo "Not installed yet, install with: " && echo "./install.sh" && exit
 cd ~/.config/config-pa-de-solminihac && git pull
 
