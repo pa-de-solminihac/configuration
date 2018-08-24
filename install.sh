2455df3c9632bd687fa2c6a9a541092b09784edb
pa-de-solminihac <pa@quai13.com>
2018-08-25 01:51:43 +0200

    feat(install): add uninstall script

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 [ -d ~/.config/config-pa-de-solminihac ] && echo "Already installed, update with:" && echo "cd ~/.config/config-pa-de-solminihac && ./update.sh" && exit
 cd ~ && [ ! -d .config ] && mkdir .config && chmod 700 .config
 git clone https://github.com/pa-de-solminihac/configuration.git ~/.config/config-pa-de-solminihac
-echo "source ~/.bashrc_common" >> ~/.bashrc && ln -s ~/.config/config-pa-de-solminihac/bash/.bashrc_common ~/.bashrc_common
-echo "source ~/.vimrc_common" >> ~/.vimrc && ln -s ~/.config/config-pa-de-solminihac/vim/.vimrc_common ~/.vimrc_common
+echo 'source ~/.bashrc_common # installed by config-pa-de-solminihac' >> ~/.bashrc && ln -s ~/.config/config-pa-de-solminihac/bash/.bashrc_common ~/.bashrc_common
+echo 'source ~/.vimrc_common " installed by config-pa-de-solminihac' >> ~/.vimrc && ln -s ~/.config/config-pa-de-solminihac/vim/.vimrc_common ~/.vimrc_common
 mkdir -p bin && ln -s ~/.config/config-pa-de-solminihac/bin/diffconflicts ~/bin/
---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+[ ! -d ~/.config/config-pa-de-solminihac ] && echo "Not installed yet, install with: " && echo "./install.sh" && exit
+sed -i 's/.*# installed by config-pa-de-solminihac/d' ~/.bashrc
+sed -i 's/source ~\/.bashrc_common/d' >> ~/.bashrc
+rm -f ~/.bashrc_common
+sed -i 's/.*# installed by config-pa-de-solminihac/d' ~/.vimrc
+sed -i 's/source ~\/.vimrc_common/d' >> ~/.vimrc
+rm -f ~/.vimrc_common
+rm -f ~/bin/diffconflicts
+rmdir ~/bin 2> /dev/null
+rm -rf ~/.config/config-pa-de-solminihac
+rmdir ~/.config 2> /dev/null
