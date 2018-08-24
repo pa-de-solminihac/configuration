add59c1b8664dbd5aa53b9b8ee048b5428c13034
pa-de-solminihac <pa@quai13.com>
2018-08-25 01:39:59 +0200

    fix(install): basic checks in install.sh and update.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-[ -d "~/.config/config-pa-de-solminihac" ] && echo "Already installed, update with:" && echo "cd ~/.config/config-pa-de-solminihac && ./update.sh" && exit
-cd ~ && [ ! -d ".config" ] && mkdir .config && chmod 700 .config
+[ -d ~/.config/config-pa-de-solminihac ] && echo "Already installed, update with:" && echo "cd ~/.config/config-pa-de-solminihac && ./update.sh" && exit
+cd ~ && [ ! -d .config ] && mkdir .config && chmod 700 .config
 git clone https://github.com/pa-de-solminihac/configuration.git ~/.config/config-pa-de-solminihac
 echo "source ~/.bashrc_common" >> ~/.bashrc && ln -s ~/.config/config-pa-de-solminihac/bash/.bashrc_common ~/.bashrc_common
 echo "source ~/.vimrc_common" >> ~/.vimrc && ln -s ~/.config/config-pa-de-solminihac/vim/.vimrc_common ~/.vimrc_common
