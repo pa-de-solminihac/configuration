58083d3096a0979d7d7a315b416b0e7814da4653
pa-de-solminihac <pa@quai13.com>
2018-08-25 02:00:45 +0200

    fix(install): fix uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 [ ! -d ~/.config/config-pa-de-solminihac ] && echo "Not installed yet, install with: " && echo "./install.sh" && exit
-sed -i 's/.*# installed by config-pa-de-solminihac/d' ~/.bashrc
-sed -i 's/source ~\/.bashrc_common/d' >> ~/.bashrc
+sed -i '/.*# installed by config-pa-de-solminihac/d' ~/.bashrc
+sed -i '/source ~\/.bashrc_common/d' >> ~/.bashrc
 rm -f ~/.bashrc_common
-sed -i 's/.*# installed by config-pa-de-solminihac/d' ~/.vimrc
-sed -i 's/source ~\/.vimrc_common/d' >> ~/.vimrc
+sed -i '/.*# installed by config-pa-de-solminihac/d' ~/.vimrc
+sed -i '/source ~\/.vimrc_common/d' >> ~/.vimrc
 rm -f ~/.vimrc_common
 rm -f ~/bin/diffconflicts
 rmdir ~/bin 2> /dev/null
--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 [ ! -d ~/.config/config-pa-de-solminihac ] && echo "Not installed yet, install with: " && echo "./install.sh" && exit
 cd ~/.config/config-pa-de-solminihac && git pull
-
